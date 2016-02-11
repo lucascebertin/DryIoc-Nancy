a482a19ba2d2ad4342c493a0dbc649656260bfd4
lcssk8board <lucas.ce.bertin@gmail.com>
2016-02-11 18:50:02 -0200

    Integration app for nancy and dryioc bootstrapper

---
+++ source/Nancy.Bootstrappers.DryIoc/Nancy.SelfHosted.HelloWorld/Modules/HomeModule.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+
+namespace Nancy.SelfHosted.HelloWorld.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        private readonly ITest _testInjected;
+
+        public HomeModule(ITest test) : base("/home")
+        {
+            _testInjected = test;
+
+            Get["/"] = _ =>
+            {
+                var viewBag = new DynamicDictionary();
+                viewBag.Add("test", 1);
+                return View["home/index", viewBag];
+            };
+
+            Get["/index", runAsync: true] = async (_, token) =>
+            {
+                await Task.Delay(1000);
+                return "123";
+            };
+
+            Get["/list", runAsync: true] = async (_, token) =>
+            {
+                await Task.Delay(1);
+                return 500;
+            };
+
+            Get["/edit", runAsync: true] = async (_, token) =>
+            {
+                await Task.Delay(1);
+                return 404;
+            };
+        }
+    }
+}
