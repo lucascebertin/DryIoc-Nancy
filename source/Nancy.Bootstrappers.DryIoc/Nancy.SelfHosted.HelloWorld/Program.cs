0943f02b99fb912c2f1a19ba5fd4a40d97d5a531
lcssk8board <lucas.ce.bertin@gmail.com>
2016-02-11 20:21:51 -0200

    Changed bootstrapper to separated project

--- source/Nancy.Bootstrappers.DryIoc/Nancy.SelfHosted.HelloWorld/Program.cs
+++ source/Nancy.Bootstrappers.DryIoc/Nancy.SelfHosted.HelloWorld/Program.cs
@@ -1,10 +1,8 @@
 ﻿using DryIoc;
-using Nancy.Bootstrapper;
 using Nancy.Hosting.Self;
 using System;
-using Nancy.Diagnostics;
-using System.Collections.Generic;
 using System.Diagnostics;
+using Nancy.Bootstrappers.DryIoc;
 
 namespace Nancy.SelfHosted.HelloWorld
 {
@@ -22,172 +20,6 @@ namespace Nancy.SelfHosted.HelloWorld
         }
     }
 
-    public abstract class CustomBootstrapper : NancyBootstrapperWithRequestContainerBase<IContainer>
-    {
-        private List<Type> _moduleTypes;
-
-        protected override IContainer CreateRequestContainer(NancyContext context)
-        {
-            return ApplicationContainer.OpenScope();
-        }
-
-        protected override IEnumerable<INancyModule> GetAllModules(IContainer container)
-        {
-            return container.Resolve<IEnumerable<INancyModule>>();
-        }
-
-        protected override IContainer GetApplicationContainer()
-        {
-            return new Container(rules =>
-                rules.With(FactoryMethod.ConstructorWithResolvableArguments)
-            );
-        }
-
-        protected override IEnumerable<IApplicationStartup> GetApplicationStartupTasks()
-        {
-            return ApplicationContainer.Resolve<IEnumerable<IApplicationStartup>>();
-        }
-
-        protected override IDiagnostics GetDiagnostics()
-        {
-            return ApplicationContainer.Resolve<IDiagnostics>();
-        }
-
-        protected override INancyEngine GetEngineInternal()
-        {
-            return ApplicationContainer.Resolve<INancyEngine>();
-        }
-
-        protected override INancyModule GetModule(IContainer container, Type moduleType)
-        {
-            var typeFullName = moduleType.FullName;
-
-            if (container.IsRegistered<INancyModule>(serviceKey: typeFullName))
-                return container.Resolve<INancyModule>(serviceKey: typeFullName);
-
-            var instance = (INancyModule)container.New(moduleType);
-            container.RegisterInstance(instance, serviceKey: typeFullName);
-
-            return instance;
-        }
-
-        protected override IEnumerable<IRegistrations> GetRegistrationTasks()
-        {
-            return ApplicationContainer.Resolve<IEnumerable<IRegistrations>>();
-        }
-
-        protected override IEnumerable<IRequestStartup> RegisterAndGetRequestStartupTasks(IContainer container, Type[] requestStartupTypes)
-        {
-            foreach (var requestStartupType in requestStartupTypes)
-                container.Register(
-                    serviceType: typeof(IRequestStartup),
-                    implementationType: requestStartupType,
-                    reuse: Reuse.Singleton
-                );
-
-            return container.Resolve<IEnumerable<IRequestStartup>>();
-        }
-
-        protected override void RegisterBootstrapperTypes(IContainer applicationContainer)
-        {
-            applicationContainer.RegisterInstance<INancyModuleCatalog>(this);
-        }
-
-        protected override void RegisterCollectionTypes(IContainer container, IEnumerable<CollectionTypeRegistration> collectionTypeRegistrationsn)
-        {
-            foreach (var collectionTypeRegistration in collectionTypeRegistrationsn)
-            {
-                foreach (var implementationType in collectionTypeRegistration.ImplementationTypes)
-                {
-                    switch (collectionTypeRegistration.Lifetime)
-                    {
-                        case Lifetime.Transient:
-                            container.Register(
-                                collectionTypeRegistration.RegistrationType, 
-                                implementationType, 
-                                reuse: Reuse.Transient
-                            );
-                            break;
-                        case Lifetime.Singleton:
-                            container.Register(
-                                collectionTypeRegistration.RegistrationType, 
-                                implementationType, 
-                                reuse: Reuse.Singleton
-                            );
-                            break;
-                        case Lifetime.PerRequest:
-                            container.Register(
-                                collectionTypeRegistration.RegistrationType,
-                                implementationType,
-                                reuse: Reuse.InWebRequest
-                            );
-                            break;
-                        default:
-                            throw new ArgumentOutOfRangeException();
-                    }
-                }
-            }
-        }
-
-        protected override void RegisterInstances(IContainer container, IEnumerable<InstanceRegistration> instanceRegistrations)
-        {
-            foreach (var instanceRegistration in instanceRegistrations)
-                container.RegisterInstance(instanceRegistration.RegistrationType, instanceRegistration.Implementation);
-        }
-
-        protected override void RegisterRequestContainerModules(IContainer container, IEnumerable<ModuleRegistration> moduleRegistrationTypes)
-        {
-            _moduleTypes = new List<Type>();
-
-            foreach (var moduleRegistrationType in moduleRegistrationTypes)
-            {
-                _moduleTypes.Add(moduleRegistrationType.ModuleType);
-                container.Register(
-                    typeof(INancyModule), 
-                    moduleRegistrationType.ModuleType, 
-                    serviceKey: moduleRegistrationType.ModuleType.FullName, 
-                    ifAlreadyRegistered: IfAlreadyRegistered.Keep
-                );
-            }
-        }
-
-        protected override void RegisterTypes(IContainer container, IEnumerable<TypeRegistration> typeRegistrations)
-        {
-            foreach (var typeRegistration in typeRegistrations)
-            {
-                switch (typeRegistration.Lifetime)
-                {
-                    case Lifetime.Transient:
-                        container.Register(
-                            typeRegistration.RegistrationType, 
-                            typeRegistration.ImplementationType, 
-                            reuse: Reuse.Transient, 
-                            made: FactoryMethod.ConstructorWithResolvableArguments
-                        );
-                        break;
-                    case Lifetime.Singleton:
-                        container.Register(
-                            typeRegistration.RegistrationType, 
-                            typeRegistration.ImplementationType, 
-                            reuse: Reuse.Singleton, 
-                            made: FactoryMethod.ConstructorWithResolvableArguments
-                        );
-                        break;
-                    case Lifetime.PerRequest:
-                        container.Register(
-                            typeRegistration.RegistrationType,
-                            typeRegistration.ImplementationType,
-                            reuse: Reuse.InWebRequest,
-                            made: FactoryMethod.ConstructorWithResolvableArguments
-                        );
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-            }
-        }
-    }
-
     public interface ITest
     {
         void Test123();
@@ -201,7 +33,7 @@ namespace Nancy.SelfHosted.HelloWorld
         }
     }
 
-    public class Bootstrapper : CustomBootstrapper
+    public class Bootstrapper : DryIocNancyBootstrapper
     {
         protected override void ConfigureApplicationContainer(IContainer existingContainer)
         {
